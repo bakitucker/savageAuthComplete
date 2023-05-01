db3779276a751d884cee2f83d27fb171b9c2fe5b
bakitucker <bakitucker8@gmail.com>
2023-04-30 20:39:26 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://bakitucker8:baki123@savageauthcomplete.3c6vq7h.mongodb.net/savageAuthComplete?retryWrites=true&w=majority', 
+    'dbName': 'savageAuthComplete'
+};
